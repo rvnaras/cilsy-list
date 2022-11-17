27797cace7f660a374778ff62d5ef58311bab5ba
Musyaffa Adli Syauqi <musyaffadli@gmail.com>
2022-11-17 19:56:37 +0700

    Update UserList.js

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -33,7 +33,7 @@ const UserList = () => {
   return (
     <div className="columns mt-5 is-centered">
       <div className="column is-half">
-        <Banner title="DPO Ujung Berung" />
+        <Banner title="User List" />
         <Link to={`add`} className="button is-info">
           Add New
         </Link>
